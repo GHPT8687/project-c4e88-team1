f8334c3e73b48cab18f7cb240250fbd165e67082
laidiepsdtq@gmail.com <laidiepsdtq@gmail.com>
2020-08-02 15:01:19 +0700

    function

---
+++ session8_function/function(continue).js
@@ -0,0 +1,202 @@
+// // Trả về 1 gt > 20
+// let arr = [1, 20, 6, 12, 7, 10, 9, 5, 30, 50];
+// function greaterThan20 (value) {
+//     return value > 20;
+// }
+// console.log(`Số đầu tiên lớn hơn 20: ${arr.find(greaterThan20)}`);
+
+// // Trả về 1 mảng gồm các gt > 10
+// function greaterThan10 (v) {
+//     return v > 10;
+// }
+// console.log(`Các số lớn hơn 10: ${arr.filter(greaterThan10)}`);
+
+// // Trả về 1 mảng gồm các số chẵn
+// function evenNumbers(va) {
+//     return va % 2 === 0;
+// }
+// console.log(`Các số chẵn trong mảng: ${arr.filter(evenNumbers)}`);
+
+// // Mảng ban đầu có số > 5 k
+// function greaterThan5(val) {
+//     return val > 5;
+// }
+// console.log(`Mảng có số lớn hơn 5? ${arr.some(greaterThan5)}`);
+
+// // Mảng ban đầu có số chẵn k
+// console.log(`Mảng có số chẵn? ${arr.some(evenNumbers)}`);
+
+// // Tất cả giá trị của mảng là số lẻ k
+// function oddNumbers(valu) {
+//     return valu % 2 === 1;
+// }
+// console.log(`Mảng toàn số lẻ? ${arr.every(oddNumbers)}`);
+
+let phones = [
+    {
+        name: "iPhone",
+        price: 13e6,
+        brand: "Apple"
+    },
+    {
+        name: "Galaxy Note 10",
+        price: 22e6,
+        brand: "Samsung"
+    },
+    {
+        name: "Xperia",
+        price: 15e6,
+        brand: "Sony"
+    },
+    {
+        name: "iPhone Pro Max",
+        price: 56e6,
+        brand: "Apple"
+    },
+    {
+        name: "Galaxy Fold",
+        price: 50e6,
+        brand: "Samsung"
+    }
+];
+
+// 1. Tìm đt có giá > 18tr
+let moreThan18m = phones.filter(function (phone, index) {  // quan tâm cái nào thì viết ra
+    // console.log(index);                  // chưa biết làm gì thì cứ in ra để xem nó làm gì, biết rồi thì xóa bớt đi
+    return phone.price > 18e6;
+    // console.log(phone.price);             // mới đầu đặt là value, biết value in ra là đt rồi thì sửa thành phone
+});
+console.log("Điện thoại có giá lớn hơn 18 triệu");
+print(moreThan18m);
+
+function print (phones) {
+    for (i = 0; i < phones.length; i++) {
+        const phone = phones[i];
+        for (const key in phone) {
+            const value = phone[key];
+            console.log(`${key}: ${value}`);
+        }
+        console.log("=============");
+    }
+}
+
+// 2. Tìm đt có tên chứa 'iphone'
+let iphone = phones.filter(function (phone){
+    return phone.name.toLowerCase().includes('iphone');
+});
+
+console.log("Kết quả cho 'iphone'");
+print(iphone);
+
+// 3. Tìm đt có giá từ 10tr đến 20tr
+let filtered = phones.filter(function (phone) {
+    return phone.price >= 10e6 && phone.price <= 20e6;
+});
+
+console.log("Điện thoại có giá từ 10 triệu đến 20 triệu");
+print(filtered);
+
+// 4. Có đt < 18tr k?
+let iflessThan18m = phones.some(dtDuoi18tr);
+function dtDuoi18tr(phone) {
+    return phone.price < 18e6;
+}
+
+let lessThan18m = phones.filter(function (phone) {
+    return phone.price < 18e6;
+});
+
+if (iflessThan18m) {
+    console.log(`Điện thoại dưới 18 triệu`);
+    print(lessThan18m);
+}
+else {
+    console.log("Không có điện thoại dưới 18 triệu");
+    console.log("=============");
+}
+
+// 5. Tìm các đt của Apple
+let apple = phones.filter(function (phone) {
+    return phone.brand === 'Apple';
+});
+
+console.log("Điện thoại của Apple");
+print(apple);
+
+// 6. Tìm đt giá max, min
+let prices = [];
+for (let value of phones) {
+    prices.push(value.price);
+}
+
+let max = Math.max.apply(null, prices);
+let min = Math.min.apply(null, prices);
+
+let maxPhone = phones.filter(function (phone) {
+    return phone.price === max;
+});
+
+let minPhone = phones.filter(function (phone) {
+    return phone.price === min;
+});
+
+console.log(`Điện thoại có giá cao nhất`);
+print(maxPhone);
+
+console.log(`Điện thoại có giá thấp nhất`);
+print(minPhone);
+
+// 7. Tính tổng gt của all đt
+let sum = 0;
+for (let x of prices) {
+    sum += x;
+}
+console.log(`Tổng giá trị của tất cả điện thoại: ${sum}`);
+console.log("=============");
+
+// 8. Có bn đt của Sony?
+let sony = phones.filter(function (phone) {
+    return phone.brand === 'Sony';
+});
+
+console.log(`Có ${sony.length} điện thoại của Sony`);
+print(sony);
+
+// 9. Sắp xếp đt theo tên a -> z
+let lowerCase = [];
+let sortedName = [];
+
+for (let phone of phones) {
+    lowerCase.push(phone.name.toLowerCase());
+}
+
+lowerCase = lowerCase.sort();
+
+for (let i = 0; i < lowerCase.length; i++) {
+    for (let j = 0; j < phones.length; j++) {
+        if (phones[j].name.toLowerCase().includes(lowerCase[i])) {
+            sortedName.push(phones[j]);
+        }
+    }
+}
+
+console.log("Sắp xếp tên điện thoại theo thứ tự a -> z");
+print(sortedName);
+
+// 10. Sắp xếp đt theo giá cao -> thấp
+let sortedPrice = [];
+
+prices = prices.sort(function (a, b) {
+    return b - a;
+});
+
+for (i = 0; i < prices.length; i++) {
+    for (j = 0; j < phones.length; j++) {
+        if (phones[j].price === prices[i]) {
+            sortedPrice.push(phones[j]);
+        }
+    }
+}
+
+console.log("Sắp xếp đt theo giá cao -> thấp");
+print(sortedPrice);
--- session8_function/function.js
+++ session8_function/function.js
@@ -1,174 +1,203 @@
-// // function hello() {
-// //     console.log("Welcome C4EJS 88");
-// // }
-// // hello();
+// // // function hello() {
+// // //     console.log("Welcome C4EJS 88");
+// // // }
+// // // hello();
 
-// // function welcome(s) {
-// //     console.log("Welcome", s);
-// // }
-// // let xinChao = "";
-// // welcome(xinChao);
-// // welcome("Taylor");
+// // // function welcome(s) {
+// // //     console.log("Welcome", s);
+// // // }
+// // // let xinChao = "";
+// // // welcome(xinChao);
+// // // welcome("Taylor");
 
-// // let a = 10;
-// // let b = 3;
+// // // let a = 10;
+// // // let b = 3;
 
-// // function tong(a, b) {
-// //     console.log(`${a} + ${b} = ${a + b}`);
-// // }
-// // tong(a, b);
+// // // function tong(a, b) {
+// // //     console.log(`${a} + ${b} = ${a + b}`);
+// // // }
+// // // tong(a, b);
 
-// // function tich(a, b) {
-// //     console.log(`${a} * ${b} = ${a * b}`);
-// // }
-// // tich(a, b);
+// // // function tich(a, b) {
+// // //     console.log(`${a} * ${b} = ${a * b}`);
+// // // }
+// // // tich(a, b);
 
-// // function thuong(a, b) {
-// //     console.log(`${a} / ${b} = ${a / b}`);
-// // }
-// // thuong(a, b);
+// // // function thuong(a, b) {
+// // //     console.log(`${a} / ${b} = ${a / b}`);
+// // // }
+// // // thuong(a, b);
 
-// // function hieu(a, b) {
-// //     console.log(`${a} - ${b} = ${a - b}`);
-// // }
-// // hieu(a, b);
+// // // function hieu(a, b) {
+// // //     console.log(`${a} - ${b} = ${a - b}`);
+// // // }
+// // // hieu(a, b);
 
-// // function ptBac1(a, b) {
-// //     if (a === 0 && b !== 0) {
-// //         console.log("Phương trình vô nghiệm");
-// //     }
-// //     else if (a === 0 && b === 0) {
-// //         console.log("Phương trình có vô số nghiệm");
-// //     }
-// //     else {
-// //         console.log(`Phương trình có nghiệm x = ${-b / a}`);
-// //     }
-// // }
-// // ptBac1(5, -10);
-// // ptBac1(3, 0);
-// // ptBac1(0, 3);
-// // ptBac1(0, 0);
+// // // function ptBac1(a, b) {
+// // //     if (a === 0 && b !== 0) {
+// // //         console.log("Phương trình vô nghiệm");
+// // //     }
+// // //     else if (a === 0 && b === 0) {
+// // //         console.log("Phương trình có vô số nghiệm");
+// // //     }
+// // //     else {
+// // //         console.log(`Phương trình có nghiệm x = ${-b / a}`);
+// // //     }
+// // // }
+// // // ptBac1(5, -10);
+// // // ptBac1(3, 0);
+// // // ptBac1(0, 3);
+// // // ptBac1(0, 0);
 
-// // function soNguyenTo(a) {
-// //     let count = 0;
-// //     if (a < 2) {
-// //         console.log("Không phải số nguyên tố");
-// //     }
-// //     else {
-// //         for (let i = 0; i <= a; i++) {
-// //             if (a % i === 0) {
-// //                 count++;
-// //             }
-// //         }
-// //         if (count === 2) {
-// //             console.log(`${a} là số nguyên tố`);
-// //         }
-// //         else {
-// //             console.log(`${a} không là số nguyên tố`);
-// //         }
-// //     }
-// // }
+// // // function soNguyenTo(a) {
+// // //     let count = 0;
+// // //     if (a < 2) {
+// // //         console.log("Không phải số nguyên tố");
+// // //     }
+// // //     else {
+// // //         for (let i = 0; i <= a; i++) {
+// // //             if (a % i === 0) {
+// // //                 count++;
+// // //             }
+// // //         }
+// // //         if (count === 2) {
+// // //             console.log(`${a} là số nguyên tố`);
+// // //         }
+// // //         else {
+// // //             console.log(`${a} không là số nguyên tố`);
+// // //         }
+// // //     }
+// // // }
 
-// // let x = Number(prompt("Nhập 1 số"));
-// // soNguyenTo(x);
+// // // let x = Number(prompt("Nhập 1 số"));
+// // // soNguyenTo(x);
 
-// // function tinhTong(a, b) {
-// //     let c = a + b;
-// //     return c;
-// // }
+// // // function tinhTong(a, b) {
+// // //     let c = a + b;
+// // //     return c;
+// // // }
 
-// // let sum = tinhTong(10, 20);
-// // console.log(sum);
-// // console.log(`Trung bình cộng: ${sum / 2}`);
+// // // let sum = tinhTong(10, 20);
+// // // console.log(sum);
+// // // console.log(`Trung bình cộng: ${sum / 2}`);
 
-// // Nhận vào 1 số n, trả về tổng từ 1 đến n, in ra kq (ngoài function)
-// // function sum2(n) {
-// //     let sum = 0;
-// //     for (let i = 0; i <= n; i++) {
-// //        sum += i;
-// //     }
-// //     return sum;
-// // }
+// // // Nhận vào 1 số n, trả về tổng từ 1 đến n, in ra kq (ngoài function)
+// // // function sum2(n) {
+// // //     let sum = 0;
+// // //     for (let i = 0; i <= n; i++) {
+// // //        sum += i;
+// // //     }
+// // //     return sum;
+// // // }
 
-// // let n = Number(prompt("Nhập 1 số"));
-// // console.log(`Tổng các số từ 1 đến ${n}: ${sum2(n)}`);
+// // // let n = Number(prompt("Nhập 1 số"));
+// // // console.log(`Tổng các số từ 1 đến ${n}: ${sum2(n)}`);
 
-// Tính giai thừa
-function giaiThua(a) {
-    result = 1;
-    for (let i = 1; i <= a; i++) {
-        result *= i;
-    }
-    return result;
+// // Tính giai thừa
+// function giaiThua(a) {
+//     result = 1;
+//     for (let i = 1; i <= a; i++) {
+//         result *= i;
+//     }
+//     return result;
+// }
+
+// let n = Number(prompt("Nhập 1 số để tính giai thừa"));
+// console.log(`${n}! = ${giaiThua(n)}`);
+
+// // Tính giai thừa theo cách đệ quy
+// function gt (n) {
+//     if (n === 1) {
+//         return 1;
+//     }
+//     return n* gt(n - 1);
+// }
+
+// console.log(gt(10));
+
+// // Nhập vào 1 mảng các số, trả về tổng các số trong mảng
+// function tinhTong(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         sum += arr[i];
+//     }
+//     return sum;
+// }
+
+// function taoMang () {
+//     let soPhanTu = parseInt(prompt("Mảng có bao nhiêu phần tử?"));
+//     let mang = [];
+//     while (mang.length < soPhanTu) {
+//         mang.push(Number(prompt("Nhập số cho mảng")));
+//     }
+//     return mang;
+// }
+
+// console.log(`Tổng của mảng là ${tinhTong(taoMang())}`);
+
+// // Nhận vào 1 mảng các số, trả về min/max của mảng
+// function timMin(arr) {
+//     let min = arr[0];
+//     for (let i = 1; i < arr.length; i++) {
+//         if (min > arr[i]) {
+//             min = arr[i];
+//         }
+//     }
+//     return min;
+// }
+
+// function timMax(arr) {
+//     let max = arr[0];
+//     for (let i = 1; i < arr.length; i++) {
+//         if (max < arr[i]) {
+//             max = arr[i];
+//         }
+//     }
+//     return max;
+// }
+
+// console.log(`Giá trị nhỏ nhất của mảng: ${timMin(taoMang())}`);
+// console.log(`Giá trị lớn nhất của mảng: ${timMax(taoMang())}`);
+
+// // Nhận vào 1 mảng và 1 số, trả về 1 mảng mới gồm các giá trị nhỏ hơn số đã cho
+// function compared (compare_number, arr) {
+//     result = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] < compare_number) {
+//             result.push(arr[i]);
+//         }
+//     }
+//     return result;
+// }
+
+// let soSanh = Number(prompt("Nhập 1 số để so sánh"));
+// console.log(`Các giá trị trong mảng nhỏ hơn ${soSanh}: ${compared(soSanh, taoMang())}`);
+
+// Array Iteration
+let arr = [1, 20, 6, 12, 7, 10, 9, 5];
+
+let index = arr.findIndex(function (value) {    // value hoặc cái gì cũng được  // nếu cần mỗi value
+    return value === 6;     // viết condition của đề bài
+});
+
+console.log(index);
+
+arr.findIndex(function (v, i, a) {    // nếu cần cả value, index và array
+
+});
+
+arr.findIndex(function (v, i) {    // nếu cần mỗi index
+    return i > 5;
+});
+
+function giaTriBang6(v, i, a) {    // cần dùng cái nào thì viết cái đấy
+    return v === 6;                // y hệt function trên
 }
 
-let n = Number(prompt("Nhập 1 số để tính giai thừa"));
-console.log(`${n}! = ${giaiThua(n)}`);
+arr.findIndex(giaTriBang6);
 
-// Tính giai thừa theo cách đệ quy
-function gt (n) {
-    if (n === 1) {
-        return 1;
-    }
-    return n* gt(n - 1);
+// Find value = 20
+console.log(arr.find(giaTriLonHon20));
+function giaTriLonHon20(v) {         // nên tạo function mới cho dễ đọc
+    return v === 20;
 }
-
-console.log(gt(10));
-
-// Nhập vào 1 mảng các số, trả về tổng các số trong mảng
-function tinhTong(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
-    }
-    return sum;
-}
-
-function taoMang () {
-    let soPhanTu = parseInt(prompt("Mảng có bao nhiêu phần tử?"));
-    let mang = [];
-    while (mang.length < soPhanTu) {
-        mang.push(Number(prompt("Nhập số cho mảng")));
-    }
-    return mang;
-}
-
-console.log(`Tổng của mảng là ${tinhTong(taoMang())}`);
-
-// Nhận vào 1 mảng các số, trả về min/max của mảng
-function timMin(arr) {
-    let min = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-        if (min > arr[i]) {
-            min = arr[i];
-        }
-    }
-    return min;
-}
-
-function timMax(arr) {
-    let max = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-        if (max < arr[i]) {
-            max = arr[i];
-        }
-    }
-    return max;
-}
-
-console.log(`Giá trị nhỏ nhất của mảng: ${timMin(taoMang())}`);
-console.log(`Giá trị lớn nhất của mảng: ${timMax(taoMang())}`);
-
-// Nhận vào 1 mảng và 1 số, trả về 1 mảng mới gồm các giá trị nhỏ hơn số đã cho
-function compared (compare_number, arr) {
-    result = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < compare_number) {
-            result.push(arr[i]);
-        }
-    }
-    return result;
-}
-
-let soSanh = Number(prompt("Nhập 1 số để so sánh"));
-console.log(`Các giá trị trong mảng nhỏ hơn ${soSanh}: ${compared(soSanh, taoMang())}`);
