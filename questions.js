60f3816d3d062576ca50fd009695ce5c748e8d80
Lại Ngọc Điệp <67640781+laingocdiep@users.noreply.github.com>
2020-09-03 23:02:37 +0700

    phần làm đề
    chỉ mỗi làm đề, chưa có thời gian đếm ngược và lưu kết quả

--- questions.js
+++ questions.js
@@ -1,48 +1,187 @@
-let de = [
-    {"Kim loại nào sau đây không tác dụng với dung dịch CuSO4?": ['Ag', 'Mg', 'Fe', 'Al']},
-    {"Kim loại nào sau đây là kim loại kiềm?": ['Cu', 'Na', 'Mg', 'Al']},
-    {"Khí X sinh ra trong quy trình đốt nhiên liệu hóa thạch, rất độc và gây ô nhiêm môi trường. Khí X là": ['CO', 'H2', 'NH3', 'N2']},
-    {"Thủy phân este CH3CH2COOCH3, thu được ancol có công thức là": ['CH3OH', 'C3H7OH', 'C2H5OH', 'C3H5OH']},
-    {"Ở nhiệt độ thường, kim loại Fe không phản ứng với dung dịch nào sau đây?": ['NaNO3', 'HCI', 'CuSO4', 'AgNO3']},
-    {"Dung dịch chất nào sau đây làm xanh quỳ tím?": ['Metanol', 'Glixerol', 'Axit axetic', 'Metylamin']},
-    {"Chất nào sau đây có tính lưỡng tính?": ['NaNO3', 'MgCl2', 'Al(OH)3', 'Na2CO3']},
-    {"Sắt có số oxi hóa +3 trong hợp chất nào sau đây?": ['Fe(OH)2', 'Fe(NO3)2', 'Fe2(SO4)3', 'FeO']},
-    {"Chất nào sau đây có phản ứng trùng hợp?": ['Etilen', 'Etylen glicol', 'Etylamin', 'Axit axetic']},
-    {"Phản ứng nào sau đây là phản ứng nhiệt nhôm?": ['3FeO + 2Al 3Fe + Al2O3', '2Al + 2NaOH + 2H2O2NaAlO2 + 3H2', '2Al + 3H2SO4Al2(SO4)3+ 3H2', '2Al + 3CuSO4Al2(SO4)3+ 3Cu']},
-    {"Số nguyên tử cacbon trong phân tử glucozo là": ['5', '10', '6', '12']},
-    {"Ở nhiệt độ thường, kim loại nào sau đây tan hết trong nước dư?": ['Ba', 'Al', 'Fe', 'Cu']},
-    {"Chất nào sau đây được dùng đề làm mềm nước cứng có tính tạm thời?": ['CaCO3', 'MgCl2', 'NaOH', 'Fe(OH)2']},
-    {"Dung dịch KOH tác dụng với chất nào sau đây tạo ra kết tủa Fe(OH)3?": ['FeCl3', 'FeO', 'Fe2O3', 'Fe3O4']},
-    {"Chất nào sau đây là chất điện ly yếu?": ['HCI', 'KNO3', 'CH3COOH', 'NaOH']},
-    {"Thủy phân triolein có công thức (C17H33COO)3C3H5 trong dung dịch NaOH, thu được glyxerol và muối X. Công thức của X là": ['C17H35COONa', 'CH3COONa', 'C2H5COONa', 'C17H33COONa']},
-    {"Natri hidroxit (còn gọi là xút ăn da) có công thức hóa học là": ['NaOH', 'NaHCO3', 'Na2CO3', 'Na2SO4']},
-    {"Chất nào sau đây có một liên kết ba trong phân tử?": ['Metan', 'Etilen', 'Axetilen', 'Benzen']},
-    {"Chất X có công thức H2N-CH(CH3)COOH. Tên gọi của X là": ['glyxin', 'valin', 'alanin', 'lysin']},
-    {"Thành phần chính của vỏ các loại ốc, sến, sò là": ['Ca(NO3)2', 'CaCO3', 'NaCl', 'Na2CO3']}
+let myQuestions = [
+    {
+        question: "Chất nào sau đây có phản ứng trùng hợp?",
+        answers: {
+            a: 'Etilen',
+            b: 'Etylen glicol',
+            c: 'Etylamin',
+            d: 'Axit axetic'},
+        correctAnswer: 'a'
+    },
+    {
+        question: "Kim loại nào sau đây không tác dụng với dung dịch CuSO4?",
+        answers: {
+            a: 'Ag',
+            b: 'Mg',
+            c: 'Fe',
+            d: 'Al'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "Kim loại nào sau đây là kim loại kiềm?",
+        answers: {
+            a: 'Cu',
+            b: 'Na',
+            c: 'Mg',
+            d: 'Al'
+        },
+        correctAnswer: 'b'   
+    },
+    {
+        question: "Khí X sinh ra trong quy trình đốt nhiên liệu hóa thạch, rất độc và gây ô nhiêm môi trường. Khí X là",
+        answers: {
+            a: 'CO',
+            b: 'H2',
+            c: 'NH3',
+            d: 'N2'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "Ở nhiệt độ thường, kim loại Fe không phản ứng với dung dịch nào sau đây?",
+        answers: {
+            a: 'NaNO3',
+            b: 'HCl',
+            c: 'CuSO4',
+            d: 'AgNO3'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "Dung dịch chất nào sau đây làm xanh quỳ tím?",
+        answers: {
+            a: 'Metanol',
+            b: 'Glixerol',
+            c: 'Axit axetic',
+            d: 'Metylamin'
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: "Số nguyên tử cacbon trong phân tử glucozo là",
+        answers: {
+            a: '5',
+            b: '10',
+            c: '6',
+            d: '12'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "Chất nào sau đây có tính lưỡng tính?",
+        answers: {
+            a: 'NaNO3',
+            b: 'MgCl2',
+            c: 'Al(OH)3',
+            d: 'Na2CO3'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "Chất nào sau đây được dùng đề làm mềm nước cứng có tính tạm thời?",
+        answers: {
+            a: 'CaCO3',
+            b: 'MgCl2',
+            c: 'NaOH',
+            d: 'Fe(OH2)'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "Chất X có công thức H2N-CH(CH3)COOH. Tên gọi của X là",
+        answers: {
+            a: 'glyxin',
+            b: 'valin',
+            c: 'alanin',
+            d: 'lysin'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "Thủy phân este CH3CH2COOCH3, thu được ancol có công thức là",
+        answers: {
+            a: 'CH3OH',
+            b: 'C3H7OH',
+            c: 'C2H5OH',
+            d: 'C3H5OH'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "Sắt có số oxi hóa +3 trong hợp chất nào sau đây?",
+        answers: {
+            a: 'Fe(OH)2',
+            b: 'Fe(NO3)2',
+            c: 'Fe2(SO4)3',
+            d: 'FeO'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "Thành phần chính của vỏ các loại ốc, sến, sò là",
+        answers: {
+            a: 'Ca(NO3)2',
+            b: 'CaCO3',
+            c: 'NaCl',
+            d: 'Na2CO3'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "Chất nào sau đây có một liên kết ba trong phân tử?",
+        answers: {
+            a: 'Metan',
+            b: 'Etilen',
+            c: 'Axetilen',
+            d: 'Benzen'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "Chất nào sau đây là chất điện ly yếu?",
+        answers: {
+            a: 'HCl',
+            b: 'KNO3',
+            c: 'NaOH',
+            d: 'CH3COOH'
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: "Thủy phân triolein có công thức (C17H33COO)3C3H5 trong dung dịch NaOH, thu được glyxerol và muối X. Công thức của X là",
+        answers: {
+            a: 'C17H35COONa',
+            b: 'CH3COONa',
+            c: 'C2H5COONa',
+            d: 'C17H33COONa'
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: "Natri hidroxit (còn gọi là xút ăn da) có công thức hóa học là",
+        answers: {
+            a: 'NaOH',
+            b: 'NaHCO3',
+            c: 'Na2CO3',
+            d: 'Na2SO4'
+        },
+        correctAnswer: 'a'
+    },
 ];
 
-let listIndexCauHoi = [];
 let newList = [];
 let indexRandom;
+let listIndexCauHoi = [];
 while (newList.length < 10) {
     indexRandom = Math.floor((Math.random() * 100));
-    if (indexRandom < de.length) {
+    if (indexRandom < myQuestions.length) {   // đảm bảo index < số câu của đề
         listIndexCauHoi.push(indexRandom);
     }
-    newList = [...new Set(listIndexCauHoi)];
+    newList = [...new Set(listIndexCauHoi)];  // đảm bảo list k bị trùng số
 }
 
-console.log(newList);
-
-for (let i = 0; i <= newList.length; i++) {
-    let cau = newList[i];
-    for (let cauHoi in de[cau]) {
-        let dapAn = de[cau][cauHoi];
-        document.getElementById("question").innerHTML +=
-            `<li><h4>${cauHoi}</h4></li>
-            <input type="checkbox"> A. ${dapAn[0]} <br>
-            <input type="checkbox"> B. ${dapAn[1]}  <br>
-            <input type="checkbox"> C. ${dapAn[2]}  <br>
-            <input type="checkbox"> D. ${dapAn[3]}`
-    }
+let questionsDisplayed = [];
+for (let index of newList) {
+    questionsDisplayed.push(myQuestions[index]);
 }
---
+++ try2.js
@@ -0,0 +1,79 @@
+let quizContainer = document.getElementById('quiz');
+let resultsContainer = document.getElementById('results');
+let submitButton = document.getElementById('submit');
+
+function buildQuiz(){
+    // variable to store the HTML output
+  let output = [];
+
+  // for each question...
+  questionsDisplayed.forEach(
+    (currentQuestion, questionNumber) => {
+
+      // variable to store the list of possible answers
+      let answers = [];
+
+      // and for each available answer...
+      for(letter in currentQuestion.answers){
+
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
+
+      // add this question and its answers to the output
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
+      );
+    }
+  );
+
+  // finally combine our output list into one string of HTML and put it on the page
+  quizContainer.innerHTML = output.join('');
+}
+
+function showResults(){
+    // gather answer containers from our quiz
+    let answerContainers = quizContainer.querySelectorAll('.answers');
+      
+    // keep track of user's answers
+    let numCorrect = 0;
+      
+    // for each question...
+    questionsDisplayed.forEach( (currentQuestion, questionNumber) => {
+      
+      // find selected answer
+      let answerContainer = answerContainers[questionNumber];
+      let selector = `input[name=question${questionNumber}]:checked`;
+      let userAnswer = (answerContainer.querySelector(selector) || {}).value;
+      
+      // if answer is correct
+      if(userAnswer === currentQuestion.correctAnswer){
+        // add to the number of correct answers
+        numCorrect++;
+      
+        // color the answers green
+        answerContainers[questionNumber].style.color = 'lightgreen';
+      }
+      // if answer is wrong or blank
+      else{
+        // color the answers red
+        answerContainers[questionNumber].style.color = 'red';
+      }
+    });
+      
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `Your score: ${numCorrect}/${questionsDisplayed.length}`;
+}
+
+// display quiz right away
+buildQuiz();
+
+// on submit, show results
+submitButton.addEventListener('click', showResults);
